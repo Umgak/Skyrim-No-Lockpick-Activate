99fd6d14d5583570d1f3c00c60e3e4939648167e
Umgak <w79hzhv3opz6@opayq.com>
2020-03-01 16:41:06 -0600

    Should be version independent now so long as meh's address library is kept up-to-date

---
+++ README.md
@@ -0,0 +1,9 @@
+# RememberLockpickAngle
+An SKSE plugin that disables automatic activation of containers & doors when unlocked.
+
+## Build Dependencies
+* [SKSE64](https://skse.silverlock.org)
+
+## End User Dependencies
+* [SKSE64](https://skse.silverlock.org/)
+* [Address Library for SKSE Plugins](https://www.nexusmods.com/skyrimspecialedition/mods/32444)
--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,44 @@
 #include "skse64_common/SafeWrite.h" // SafeWriteBuf
 #include "skse64_common/Relocation.h" // RelocPtr
 
-RelocPtr <uintptr_t> ContainerWithKeyAddr(0x22B834);
-RelocPtr <uintptr_t> LockpickActivateAddr(0x897FCE);
+#include "versiondb.h" //meh's magical versioning header
+
+unsigned long long LockpickActivateOffset = 0;
+unsigned long long ContainerWithKeyOffset = 0;
+
+bool InitializeOffsets()
+{
+	// Allocate on stack so it will be unloaded when we exit this function.
+	// No need to have the whole database loaded and using up memory for no reason.
+	VersionDb db;
+
+	// Load database with current executable version.
+	if (!db.Load())
+	{
+		_FATALERROR("[FATAL ERROR] Failed to load version database for current executable!");
+		return false;
+	}
+	else
+	{
+		_MESSAGE("[MESSAGE] Loaded database for %s version %s.", db.GetModuleName().c_str(), db.GetLoadedVersionString().c_str());
+	}
+
+	// This offset does not include base address. Actual address would be ModuleBase + MyOffset.
+	if (!db.FindOffsetById(17485, ContainerWithKeyOffset))
+	{
+		_FATALERROR("[FATAL ERROR] Failed to find offset for activating container with key!");
+		return false;
+	}
+	if (!db.FindOffsetById(51088, LockpickActivateOffset))
+	{
+		_FATALERROR("[FATAL ERROR] Failed to find offset for activating container with lockpick!");
+		return false;
+	}
+
+	// Everything was successful.
+	db.Clear();
+	return true;
+}
 
 extern "C" {
 	bool SKSEPlugin_Query(const SKSEInterface* skse, PluginInfo* info)
@@ -25,19 +61,11 @@ extern "C" {
 		info->version = NLA_VERSION_MAJOR;
 		
 		if (skse->isEditor) {
-			_FATALERROR("[FATAL ERROR] Loaded in editor, marking as incompatible!\n");
+			_FATALERROR("[FATAL ERROR] Loaded in editor, marking as incompatible!");
 			return false;
 		} 
-		
-		switch (skse->runtimeVersion) {
-		case RUNTIME_VERSION_1_5_73:
-		case RUNTIME_VERSION_1_5_80:
-		case RUNTIME_VERSION_1_5_97:
-			break;
-		default:
-			_MESSAGE("This plugin is not compatible with this version of game.");
-			return false;
-		}
+		//functions referenced by this program exist in all versions of the library
+		//therefore, no version check is actually required
 
 		return true;
 	}
@@ -45,12 +73,24 @@ extern "C" {
 
 	bool SKSEPlugin_Load(const SKSEInterface* skse)
 	{
-		SafeWriteBuf(ContainerWithKeyAddr.GetUIntPtr(), (void*)"\x90\x90\x90\x90\x90", 5); // container with key
-		SafeWriteBuf(LockpickActivateAddr.GetUIntPtr(), (void*)"\x90\x90\x90\x90\x90", 5); // lockpick activate
+		if (InitializeOffsets())
+		{
+			RelocPtr <uintptr_t> ContainerWithKeyAddr(ContainerWithKeyOffset+0x164);
+			RelocPtr <uintptr_t> LockpickActivateAddr(LockpickActivateOffset+0x1BE);
+			SafeWriteBuf(ContainerWithKeyAddr.GetUIntPtr(), (void*)"\x90\x90\x90\x90\x90", 5); // container with key
+			SafeWriteBuf(LockpickActivateAddr.GetUIntPtr(), (void*)"\x90\x90\x90\x90\x90", 5); // lockpick activate
 
-		_MESSAGE("[MESSAGE] No Lockpick Activate loaded");
-
+			_MESSAGE("[MESSAGE] No Lockpick Activate loaded successfully.");
+		}
+		else
+		{
+			_FATALERROR("[FATAL ERROR] Unable to find offsets for this version of the game!");
+			_FATALERROR("[FATAL ERROR] Make sure you install meh321's Address Library for SKSE Plugins!");
+			_FATALERROR("[FATAL ERROR] These can be found at: https://www.nexusmods.com/skyrimspecialedition/mods/32444");
+			return false;
+		}
 		return true;
 	}
 
 }
+
